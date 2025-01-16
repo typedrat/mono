e2f7eb9e1d1fac4ecb19bf7d63374d1311256bab
Erik Arvidsson <arv@roci.dev>
2025-01-16 12:40:43 +0100

    feat(zero): Add a BTreeSet implementation (#3544)
    This is based on the btree npm package with extra code removed and a few
    things simplified and cleaned up.
    
    This also removes packages/btree

--- packages/zql/src/ivm/memory-storage.ts
+++ packages/zql/src/ivm/memory-storage.ts
@@ -1,41 +1,50 @@
-import {BTree} from '../../../btree/src/mod.js';
+import {compareUTF8} from 'compare-utf8';
+import {BTreeSet} from '../../../shared/src/btree-set.js';
 import type {JSONValue} from '../../../shared/src/json.js';
 import type {Storage} from './operator.js';
 import type {Stream} from './stream.js';
+
+type Entry = [key: string, value: JSONValue];
+
+function comparator(a: Entry, b: Entry): number {
+  return compareUTF8(a[0], b[0]);
+}
 
 /**
  * MemoryStorage is a simple in-memory implementation of `Storage` for use
  * on the client and in tests.
  */
 export class MemoryStorage implements Storage {
-  #data: BTree<string, JSONValue> = new BTree();
+  #data: BTreeSet<Entry> = new BTreeSet(comparator);
 
   set(key: string, value: JSONValue) {
-    this.#data.add(key, value);
+    this.#data.add([key, value]);
   }
 
   get(key: string, def?: JSONValue): JSONValue | undefined {
-    const r = this.#data.get(key);
+    const r = this.#data.get([key, null]);
     if (r !== undefined) {
-      return r;
+      return r[1];
     }
     return def;
   }
 
   del(key: string) {
-    this.#data.delete(key);
+    this.#data.delete([key, null]);
   }
 
   *scan(options?: {prefix: string}): Stream<[string, JSONValue]> {
-    for (const [key, value] of this.#data.entries(options?.prefix)) {
-      if (options && !key.startsWith(options.prefix)) {
+    for (const entry of this.#data.valuesFrom(
+      options && [options.prefix, null],
+    )) {
+      if (options && !entry[0].startsWith(options.prefix)) {
         return;
       }
-      yield [key, value];
+      yield entry;
     }
   }
 
   cloneData(): Record<string, JSONValue> {
-    return structuredClone(Object.fromEntries(this.#data.entries()));
+    return structuredClone(Object.fromEntries(this.#data.values()));
   }
 }
