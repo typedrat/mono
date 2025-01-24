08a10b28d12d8764c25d9d12ac2f24493ad840f0
Erik Arvidsson <arv@roci.dev>
2025-01-24 15:30:01 +0100

    fix(zero-react): Export transitive types (#3594)
    https://discord.com/channels/830183651022471199/1288232858795769917/1331312171212210207
    
    This is another issue where `declations: true` is causing issues because
    we are not exporting all the types we are referencing.

--- packages/zero-react/src/mod.ts
+++ packages/zero-react/src/mod.ts
@@ -1,2 +1,16 @@
-export {useQuery} from './use-query.jsx';
-export {ZeroProvider, createUseZero, useZero} from './use-zero.jsx';
+export type {Expand} from '../../shared/src/expand.js';
+export type {PrimaryKey} from '../../zero-protocol/src/primary-key.js';
+export type {Schema} from '../../zero-schema/src/builder/schema-builder.js';
+export type {
+  RelationshipsSchema,
+  SchemaValue,
+  TableSchema,
+} from '../../zero-schema/src/table-schema.js';
+export type {HumanReadable} from '../../zql/src/query/query.js';
+export type {ResultType} from '../../zql/src/query/typed-view.js';
+export {
+  useQuery,
+  type QueryResult,
+  type QueryResultDetails,
+} from './use-query.jsx';
+export {createUseZero, useZero, ZeroProvider} from './use-zero.jsx';
