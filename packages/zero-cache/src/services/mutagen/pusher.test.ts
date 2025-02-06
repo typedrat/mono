c903682b7e6ca409a711a140a33894afee7e98ba
Matt Wonlaw <mlaw@roci.dev>
2025-02-05 21:08:53 -0500

    feat(zero-cache): pusher service
    Service that sits between zero-client and the user's API server. Receives pushes and forwards them, in order, to the user's API server. Does batching in the case where a client gets ahead of the API server.

---
+++ packages/zero-cache/src/services/mutagen/pusher.test.ts
@@ -0,0 +1,272 @@
+import {describe, expect, test, vi} from 'vitest';
+import {combinePushes, PusherService, Queue} from './pusher.ts';
+import type {Mutation, PushBody} from '../../../../zero-protocol/src/push.ts';
+import {createSilentLogContext} from '../../../../shared/src/logging-test-utils.ts';
+import {resolver} from '@rocicorp/resolver';
+
+describe('queue', () => {
+  test('a consumer blocks until tasks are available', async () => {
+    const queue = new Queue<number>();
+    const promise = queue.awaitTasks().then(() => {
+      const tasks = queue.drain();
+      expect(tasks).toEqual([1]);
+    });
+    queue.enqueue(1);
+    await promise;
+  });
+
+  test('drain will get all tasks that were accumulated in the prior tick', async () => {
+    const queue = new Queue<number>();
+    const promise = queue.awaitTasks().then(() => {
+      const tasks = queue.drain();
+      expect(tasks).toEqual([1, 2, 3]);
+    });
+    queue.enqueue(1);
+    queue.enqueue(2);
+    queue.enqueue(3);
+    await promise;
+  });
+
+  test('a consumer is called if tasks are already available', async () => {
+    const queue = new Queue<number>();
+    queue.enqueue(1);
+    const promise = queue.awaitTasks().then(() => {
+      const tasks = queue.drain();
+      expect(tasks).toEqual([1]);
+    });
+    await promise;
+  });
+
+  test('drain will get all tasks that were accumulated in the prior tick | 2', async () => {
+    const queue = new Queue<number>();
+    queue.enqueue(1);
+    const promise = queue.awaitTasks().then(() => {
+      const tasks = queue.drain();
+      expect(tasks).toEqual([1, 2, 3]);
+    });
+    queue.enqueue(2);
+    queue.enqueue(3);
+    await promise;
+  });
+});
+
+describe('combine pushes', () => {
+  test('empty array', () => {
+    const [pushes, terminate] = combinePushes([]);
+    expect(pushes).toEqual([]);
+    expect(terminate).toBe(false);
+  });
+
+  test('same JWT for all pushes', () => {
+    const [pushes, terminate] = combinePushes([
+      {
+        push: makePush(1),
+        jwt: 'a',
+      },
+      {
+        push: makePush(1),
+        jwt: 'a',
+      },
+      {
+        push: makePush(1),
+        jwt: 'a',
+      },
+    ]);
+    expect(pushes).toHaveLength(1);
+    expect(terminate).toBe(false);
+    expect(pushes[0].push.mutations).toHaveLength(3);
+  });
+
+  test('different JWT groups', () => {
+    const [pushes, terminate] = combinePushes([
+      {
+        push: makePush(1),
+        jwt: 'a',
+      },
+      {
+        push: makePush(1),
+        jwt: 'a',
+      },
+      {
+        push: makePush(1),
+        jwt: 'c',
+      },
+      {
+        push: makePush(1),
+        jwt: 'b',
+      },
+      {
+        push: makePush(1),
+        jwt: 'b',
+      },
+      {
+        push: makePush(1),
+        jwt: 'c',
+      },
+    ]);
+    expect(pushes).toHaveLength(4);
+    expect(terminate).toBe(false);
+    expect(pushes[0].push.mutations).toHaveLength(2);
+    expect(pushes[0].jwt).toBe('a');
+    expect(pushes[1].push.mutations).toHaveLength(1);
+    expect(pushes[1].jwt).toBe('c');
+    expect(pushes[2].push.mutations).toHaveLength(2);
+    expect(pushes[2].jwt).toBe('b');
+    expect(pushes[3].push.mutations).toHaveLength(1);
+    expect(pushes[3].jwt).toBe('c');
+  });
+
+  test('stop', () => {
+    const [pushes, terminate] = combinePushes(['stop']);
+    expect(pushes).toEqual([]);
+    expect(terminate).toBe(true);
+  });
+
+  test('stop after pushes', () => {
+    const [pushes, terminate] = combinePushes([
+      {
+        push: makePush(1),
+        jwt: 'a',
+      },
+      {
+        push: makePush(1),
+        jwt: 'a',
+      },
+      'stop',
+    ]);
+    expect(pushes).toHaveLength(1);
+    expect(terminate).toBe(true);
+  });
+
+  test('stop in the middle', () => {
+    const [pushes, terminate] = combinePushes([
+      {
+        push: makePush(1),
+        jwt: 'a',
+      },
+      'stop',
+      {
+        push: makePush(1),
+        jwt: 'a',
+      },
+    ]);
+    expect(pushes).toHaveLength(1);
+    expect(terminate).toBe(true);
+  });
+});
+
+const lc = createSilentLogContext();
+describe('pusher service', () => {
+  test('the service can be stopped', async () => {
+    const pusher = new PusherService(
+      lc,
+      'cgid',
+      'http://exmaple.com',
+      undefined,
+    );
+    let shutDown = false;
+    void pusher.run().then(() => {
+      shutDown = true;
+    });
+    await pusher.stop();
+    expect(shutDown).toBe(true);
+  });
+
+  test('the service sets authorization headers', async () => {
+    const fetch = (global.fetch = vi.fn());
+    fetch.mockResolvedValue({
+      ok: true,
+    });
+
+    const pusher = new PusherService(
+      lc,
+      'cgid',
+      'http://exmaple.com',
+      'api-key',
+    );
+    void pusher.run();
+
+    pusher.enqueuePush(makePush(1), 'jwt');
+
+    await pusher.stop();
+
+    expect(fetch.mock.calls[0][1]?.headers).toEqual({
+      'Content-Type': 'application/json',
+      'X-Api-Key': 'api-key',
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      'Authorization': 'Bearer jwt',
+    });
+
+    fetch.mockReset();
+  });
+
+  test('the service correctly batches pushes when the API server is delayed', async () => {
+    const fetch = (global.fetch = vi.fn());
+    const apiServerReturn = resolver();
+    fetch.mockImplementation(async (_url: string, _options: RequestInit) => {
+      await apiServerReturn.promise;
+    });
+
+    const pusher = new PusherService(
+      lc,
+      'cgid',
+      'http://exmaple.com',
+      'api-key',
+    );
+
+    void pusher.run();
+    pusher.enqueuePush(makePush(1), 'jwt');
+    // release control of the loop so the push can be sent
+    await Promise.resolve();
+
+    // We should have sent the first push
+    expect(fetch.mock.calls).toHaveLength(1);
+    expect(JSON.parse(fetch.mock.calls[0][1].body).mutations).toHaveLength(1);
+
+    // We have not resolved the API server yet so these should stack up
+    pusher.enqueuePush(makePush(1), 'jwt');
+    await Promise.resolve();
+    pusher.enqueuePush(makePush(1), 'jwt');
+    await Promise.resolve();
+    pusher.enqueuePush(makePush(1), 'jwt');
+    await Promise.resolve();
+
+    // no new pushes sent yet since we are still waiting on the user's API server
+    expect(fetch.mock.calls).toHaveLength(1);
+
+    // let the API server go
+    apiServerReturn.resolve();
+    // wait for the pusher to finish
+    await new Promise(resolve => {
+      setTimeout(resolve, 0);
+    });
+
+    // We sent all the pushes in one batch
+    expect(JSON.parse(fetch.mock.calls[1][1].body).mutations).toHaveLength(3);
+    expect(fetch.mock.calls).toHaveLength(2);
+  });
+});
+
+let timestamp = 0;
+let id = 0;
+function makePush(numMutations: number): PushBody {
+  return {
+    clientGroupID: 'cgid',
+    mutations: Array.from({length: numMutations}, makeMutation),
+    pushVersion: 1,
+    requestID: 'rid',
+    schemaVersion: 1,
+    timestamp: ++timestamp,
+  };
+}
+
+function makeMutation(): Mutation {
+  return {
+    type: 'custom',
+    args: [],
+    clientID: 'cid',
+    id: ++id,
+    name: 'n',
+    timestamp: ++timestamp,
+  } as const;
+}
---
+++ packages/zero-cache/src/services/mutagen/pusher.ts
@@ -0,0 +1,225 @@
+import type {PushBody} from '../../../../zero-protocol/src/push.ts';
+import type {MutationError} from './mutagen.ts';
+import type {Service} from '../service.ts';
+import * as ErrorKind from '../../../../zero-protocol/src/error-kind-enum.ts';
+import {must} from '../../../../shared/src/must.ts';
+import type {LogContext} from '@rocicorp/logger';
+import {assert} from '../../../../shared/src/asserts.ts';
+
+/**
+ * Receives push messages from zero-client and forwards
+ * them the the user's API server.
+ *
+ * If the user's API server is taking too long to process
+ * the push, the PusherService will add the push to a queue
+ * and send pushes in bulk the next time the user's API server
+ * is available.
+ *
+ * - One PusherService exists per client group.
+ * - Mutations for a given client are always sent in-order
+ * - Mutations for different clients in the same group may be interleaved
+ */
+export class PusherService implements Service {
+  readonly id: string;
+  readonly #pusher: Pusher;
+  readonly #queue: Queue<PusherEntryOrStop>;
+  #stopped: Promise<void> | undefined;
+
+  constructor(
+    lc: LogContext,
+    clientGroupID: string,
+    pushUrl: string,
+    apiKey: string | undefined,
+  ) {
+    this.#queue = new Queue();
+    this.#pusher = new Pusher(lc, pushUrl, apiKey, this.#queue);
+    this.id = clientGroupID;
+  }
+
+  enqueuePush(push: PushBody, jwt: string | undefined) {
+    this.#queue.enqueue({push, jwt});
+  }
+
+  run(): Promise<void> {
+    this.#stopped = this.#pusher.run();
+    return this.#stopped;
+  }
+
+  stop(): Promise<void> {
+    void this.#queue.enqueue('stop');
+    return must(this.#stopped, 'Stop was called before `run`');
+  }
+}
+
+type PusherEntry = {
+  push: PushBody;
+  jwt: string | undefined;
+};
+type PusherEntryOrStop = PusherEntry | 'stop';
+
+/**
+ * Awaits items in the queue then drains and sends them all
+ * to the user's API server.
+ */
+class Pusher {
+  readonly #pushURL: string;
+  readonly #apiKey: string | undefined;
+  readonly #queue: Queue<PusherEntryOrStop>;
+  readonly #lc: LogContext;
+
+  constructor(
+    lc: LogContext,
+    pushURL: string,
+    apiKey: string | undefined,
+    queue: Queue<PusherEntryOrStop>,
+  ) {
+    this.#pushURL = pushURL;
+    this.#apiKey = apiKey;
+    this.#queue = queue;
+    this.#lc = lc.withContext('component', 'pusher');
+  }
+
+  async run() {
+    for (;;) {
+      await this.#queue.awaitTasks();
+      const tasks = this.#queue.drain();
+      const [pushes, terminate] = combinePushes(tasks);
+      for (const push of pushes) {
+        await this.#processPush(push);
+      }
+
+      if (terminate) {
+        break;
+      }
+    }
+  }
+
+  async #processPush(entry: PusherEntry): Promise<MutationError | undefined> {
+    const headers: Record<string, string> = {
+      'Content-Type': 'application/json',
+    };
+    if (this.#apiKey) {
+      headers['X-Api-Key'] = this.#apiKey;
+    }
+    if (entry.jwt) {
+      headers['Authorization'] = `Bearer ${entry.jwt}`;
+    }
+
+    try {
+      const response = await fetch(this.#pushURL, {
+        method: 'POST',
+        headers,
+        body: JSON.stringify(entry.push),
+      });
+      // TODO: handle more varied response types from the user's API server
+      if (!response.ok) {
+        return [
+          ErrorKind.MutationFailed,
+          `API server failed to process mutation: ${response.status} ${response.statusText}`,
+        ];
+      }
+    } catch (e) {
+      // We do not kill the pusher on error.
+      // If the user's API server is down, the mutations will never be acknowledged
+      // and the client will eventually retry.
+      this.#lc.error?.('failed to push', e);
+      return [
+        ErrorKind.MutationFailed,
+        e instanceof Error ? e.message : 'unknown error',
+      ];
+    }
+
+    return undefined;
+  }
+}
+
+/**
+ * Scans over the array of pushes and puts consecutive pushes with the same JWT
+ * into a single push.
+ *
+ * If a 'stop' is encountered, the function returns the accumulated pushes up
+ * to that point and a boolean indicating that the pusher should stop.
+ *
+ * Exported for testing.
+ *
+ * Future optimization: every unique clientID will have the same JWT for all of its
+ * pushes. Given that, we could combine pushes across clientIDs which would
+ * create less fragmentation in the case where mutations among clients are interleaved.
+ */
+export function combinePushes(
+  entries: readonly PusherEntryOrStop[],
+): [PusherEntry[], boolean] {
+  const ret: PusherEntry[] = [];
+
+  for (const entry of entries) {
+    if (entry === 'stop') {
+      return [ret, true] as const;
+    }
+
+    if (ret.length === 0) {
+      ret.push(entry);
+      continue;
+    }
+
+    const last = ret[ret.length - 1];
+    if (last.jwt === entry.jwt) {
+      last.push.mutations.push(...entry.push.mutations);
+    } else {
+      ret.push(entry);
+    }
+  }
+
+  return [ret, false] as const;
+}
+
+/**
+ * A queue that allows draining all tasks which have accumulated.
+ * A queue instance must only be used by a single producer and a single
+ * consumer.
+ *
+ * Exported for testing.
+ */
+export class Queue<T> {
+  #tasks: T[];
+  #resolveWaiter: (() => void) | undefined;
+
+  constructor() {
+    this.#tasks = [];
+  }
+
+  enqueue(entry: T): void {
+    this.#tasks.push(entry);
+    if (this.#resolveWaiter) {
+      const resolveWaiter = this.#resolveWaiter;
+      this.#resolveWaiter = undefined;
+      resolveWaiter();
+    }
+  }
+
+  /**
+   * Wait for tasks to be available.
+   */
+  awaitTasks(): Promise<void> {
+    if (this.#tasks.length > 0) {
+      return Promise.resolve();
+    }
+
+    return new Promise(resolve => {
+      assert(
+        this.#resolveWaiter === undefined,
+        'cannot await tasks if already waiting',
+      );
+      this.#resolveWaiter = resolve;
+    });
+  }
+
+  /**
+   * Get all tasks which have accumulated
+   * since the last call to `drain`.
+   */
+  drain(): T[] {
+    const ret = this.#tasks;
+    this.#tasks = [];
+    return ret;
+  }
+}
