typedrat/mono

commit c952b11ece8d7384005d2a90fe8c50124d0865f4

refactor(zql): callable relationships (#3629)
The `Exist` operators `#fetchNodeForRow` is quite expensive and is a work around for the fact that `Node`'s relationships can only be consumed a single time.  In order to read the relevant relationship, `Exist` has to fetch from source the parent `Node`.   This fetch is pure overhead, we are fetching a row we already have.

This change updates `Node`'s relationships to be callable, so that they can be consumed multiple times.

`Node`'s type changes from 

```
export type Node = {
  row: Row;
  relationships: Record<string, Stream<Node>>;
};
```

to 

```
export type Node = {
  row: Row;
  relationships: Record<string, () => Stream<Node>>;
};
```

This change also updates `ChildChange` to have a `node: Node` rather than a `row: Row` so that the relationships needed by `Exist` are also available on `ChildChange`.  Now all `Change` types use `Node` rather than `Row`.

Finally this addresses an issue with cleanup where Node's filtered by `Exists` or `Filter` were not being drained (as they never reach view-apply-change which does the draining).  Added draining to cleanup in `Exists` and `Filter` for `Node`s not yielded to downstream.

packages/zql/src/ivm/join.ts
@@ -207,7 +207,11 @@ export class Join implements Input {
       for (const parentNode of parentNodes) {
         const childChange: ChildChange = {
           type: 'child',
-          row: parentNode.row,
+          node: this.#processParentNode(
+            parentNode.row,
+            parentNode.relationships,
+            'fetch',
+          ),
           child: {
             relationshipName: this.#relationshipName,
             change,
@@ -223,7 +227,7 @@ export class Join implements Input {
         pushChildChange(change.node.row, change);
         break;
       case 'child':
-        pushChildChange(change.row, change);
+        pushChildChange(change.node.row, change);
         break;
       case 'edit': {
         const childRow = change.node.row;
@@ -255,43 +259,56 @@ export class Join implements Input {
 
   #processParentNode(
     parentNodeRow: Row,
-    parentNodeRelations: Record<string, Stream<Node>>,
+    parentNodeRelations: Record<string, () => Stream<Node>>,
     mode: ProcessParentMode,
   ): Node {
-    // This storage key tracks the primary keys seen for each unique
-    // value joined on. This is used to know when to cleanup a child's state.
-    const storageKey = makeStorageKey(
-      this.#parentKey,
-      this.#parent.getSchema().primaryKey,
-      parentNodeRow,
-    );
-
     let method: ProcessParentMode = mode;
-    if (mode === 'cleanup') {
-      const [, second] = take(
-        this.#storage.scan({
-          prefix: makeStorageKeyPrefix(parentNodeRow, this.#parentKey),
-        }),
-        2,
-      );
-      method = second ? 'fetch' : 'cleanup';
-    }
+    let storageUpdated = false;
+    const childStream = () => {
+      if (!storageUpdated) {
+        if (mode === 'cleanup') {
+          this.#storage.del(
+            makeStorageKey(
+              this.#parentKey,
+              this.#parent.getSchema().primaryKey,
+              parentNodeRow,
+            ),
+          );
+          const empty =
+            [
+              ...take(
+                this.#storage.scan({
+                  prefix: makeStorageKeyPrefix(parentNodeRow, this.#parentKey),
+                }),
+                1,
+              ),
+            ].length === 0;
+          method = empty ? 'cleanup' : 'fetch';
+        }
 
-    const childStream = this.#child[method]({
-      constraint: Object.fromEntries(
-        this.#childKey.map((key, i) => [
-          key,
-          parentNodeRow[this.#parentKey[i]],
-        ]),
-      ),
-    });
-
-    if (mode === 'fetch') {
-      this.#storage.set(storageKey, true);
-    } else {
-      mode satisfies 'cleanup';
-      this.#storage.del(storageKey);
-    }
+        storageUpdated = true;
+        // Defer the work to update storage until the child stream
+        // is actually accessed
+        if (mode === 'fetch') {
+          this.#storage.set(
+            makeStorageKey(
+              this.#parentKey,
+              this.#parent.getSchema().primaryKey,
+              parentNodeRow,
+            ),
+            true,
+          );
+        }
+      }
+      return this.#child[method]({
+        constraint: Object.fromEntries(
+          this.#childKey.map((key, i) => [
+            key,
+            parentNodeRow[this.#parentKey[i]],
+          ]),
+        ),
+      });
+    };
 
     return {
       row: parentNodeRow,
@@ -316,7 +333,10 @@ export function makeStorageKeyPrefix(row: Row, key: CompoundKey): string {
   return makeStorageKeyForValues(key.map(k => row[k]));
 }
 
-/** Exported for testing. */
+/** Exported for testing.
+ * This storage key tracks the primary keys seen for each unique
+ * value joined on. This is used to know when to cleanup a child's state.
+ */
 export function makeStorageKey(
   key: CompoundKey,
   primaryKey: PrimaryKey,

packages/zero-cache/src/auth/read-authorizer.query.test.ts
@@ -26,8 +26,7 @@ import {
   bindStaticParameters,
   buildPipeline,
 } from '../../../zql/src/builder/builder.ts';
-import {Catch} from '../../../zql/src/ivm/catch.ts';
-import type {Node} from '../../../zql/src/ivm/data.ts';
+import {Catch, type CaughtNode} from '../../../zql/src/ivm/catch.ts';
 import {MemoryStorage} from '../../../zql/src/ivm/memory-storage.ts';
 import type {Source} from '../../../zql/src/ivm/source.ts';
 import type {ExpressionBuilder} from '../../../zql/src/query/expression.ts';
@@ -1526,7 +1525,7 @@ describe('read permissions against nested paths', () => {
 
 // maps over nodes, drops all information from `row` except the id
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-function toIdsOnly(nodes: Node[]): any[] {
+function toIdsOnly(nodes: CaughtNode[]): any[] {
   return nodes.map(node => {
     return {
       id: node.row.id,

packages/zero-cache/src/services/view-syncer/pipeline-driver.ts
@@ -490,7 +490,7 @@ class Streamer {
 
       for (const [relationship, children] of Object.entries(relationships)) {
         const childSchema = must(schema.relationships[relationship]);
-        yield* this.#streamNodes(queryHash, childSchema, op, children);
+        yield* this.#streamNodes(queryHash, childSchema, op, children());
       }
     }
   }

packages/zql/src/ivm/array-view.test.ts
@@ -595,7 +595,7 @@ test('collapse', () => {
         name: 'issue',
       },
       relationships: {
-        labels: [
+        labels: () => [
           {
             row: {
               id: 1,
@@ -604,7 +604,7 @@ test('collapse', () => {
               extra: 'a',
             },
             relationships: {
-              labels: [
+              labels: () => [
                 {
                   row: {
                     id: 1,
@@ -655,9 +655,53 @@ test('collapse', () => {
 
   view.push({
     type: 'child',
-    row: {
-      id: 1,
-      name: 'issue',
+    node: {
+      row: {
+        id: 1,
+        name: 'issue',
+      },
+      relationships: {
+        labels: () => [
+          {
+            row: {
+              id: 1,
+              issueId: 1,
+              labelId: 1,
+              extra: 'a',
+            },
+            relationships: {
+              labels: () => [
+                {
+                  row: {
+                    id: 1,
+                    name: 'label',
+                  },
+                  relationships: {},
+                },
+              ],
+            },
+          },
+          {
+            row: {
+              id: 2,
+              issueId: 1,
+              labelId: 2,
+              extra: 'b',
+            },
+            relationships: {
+              labels: () => [
+                {
+                  row: {
+                    id: 2,
+                    name: 'label2',
+                  },
+                  relationships: {},
+                },
+              ],
+            },
+          },
+        ],
+      },
     },
     child: {
       relationshipName: 'labels',
@@ -671,7 +715,7 @@ test('collapse', () => {
             extra: 'b',
           },
           relationships: {
-            labels: [
+            labels: () => [
               {
                 row: {
                   id: 2,
@@ -707,9 +751,53 @@ test('collapse', () => {
   // edit the hidden row
   view.push({
     type: 'child',
-    row: {
-      id: 1,
-      name: 'issue',
+    node: {
+      row: {
+        id: 1,
+        name: 'issue',
+      },
+      relationships: {
+        labels: () => [
+          {
+            row: {
+              id: 1,
+              issueId: 1,
+              labelId: 1,
+              extra: 'a',
+            },
+            relationships: {
+              labels: () => [
+                {
+                  row: {
+                    id: 1,
+                    name: 'label',
+                  },
+                  relationships: {},
+                },
+              ],
+            },
+          },
+          {
+            row: {
+              id: 2,
+              issueId: 1,
+              labelId: 2,
+              extra: 'b2',
+            },
+            relationships: {
+              labels: () => [
+                {
+                  row: {
+                    id: 2,
+                    name: 'label2',
+                  },
+                  relationships: {},
+                },
+              ],
+            },
+          },
+        ],
+      },
     },
     child: {
       relationshipName: 'labels',
@@ -722,7 +810,17 @@ test('collapse', () => {
             labelId: 2,
             extra: 'b',
           },
-          relationships: {},
+          relationships: {
+            labels: () => [
+              {
+                row: {
+                  id: 2,
+                  name: 'label2',
+                },
+                relationships: {},
+              },
+            ],
+          },
         },
         node: {
           row: {
@@ -731,7 +829,17 @@ test('collapse', () => {
             labelId: 2,
             extra: 'b2',
           },
-          relationships: {},
+          relationships: {
+            labels: () => [
+              {
+                row: {
+                  id: 2,
+                  name: 'label2',
+                },
+                relationships: {},
+              },
+            ],
+          },
         },
       },
     },
@@ -758,19 +866,76 @@ test('collapse', () => {
   // edit the leaf
   view.push({
     type: 'child',
-    row: {
-      id: 1,
-      name: 'issue',
+    node: {
+      row: {
+        id: 1,
+        name: 'issue',
+      },
+      relationships: {
+        labels: () => [
+          {
+            row: {
+              id: 1,
+              issueId: 1,
+              labelId: 1,
+              extra: 'a',
+            },
+            relationships: {
+              labels: () => [
+                {
+                  row: {
+                    id: 1,
+                    name: 'label',
+                  },
+                  relationships: {},
+                },
+              ],
+            },
+          },
+          {
+            row: {
+              id: 2,
+              issueId: 1,
+              labelId: 2,
+              extra: 'b2',
+            },
+            relationships: {
+              labels: () => [
+                {
+                  row: {
+                    id: 2,
+                    name: 'label2x',
+                  },
+                  relationships: {},
+                },
+              ],
+            },
+          },
+        ],
+      },
     },
     child: {
       relationshipName: 'labels',
       change: {
         type: 'child',
-        row: {
-          id: 2,
-          issueId: 1,
-          labelId: 2,
-          extra: 'b2',
+        node: {
+          row: {
+            id: 2,
+            issueId: 1,
+            labelId: 2,
+            extra: 'b2',
+          },
+          relationships: {
+            labels: () => [
+              {
+                row: {
+                  id: 2,
+                  name: 'label2x',
+                },
+                relationships: {},
+              },
+            ],
+          },
         },
         child: {
           relationshipName: 'labels',
@@ -892,7 +1057,7 @@ test('collapse-single', () => {
         name: 'issue',
       },
       relationships: {
-        labels: [
+        labels: () => [
           {
             row: {
               id: 1,
@@ -900,7 +1065,7 @@ test('collapse-single', () => {
               labelId: 1,
             },
             relationships: {
-              labels: [
+              labels: () => [
                 {
                   row: {
                     id: 1,
@@ -1248,7 +1413,7 @@ test('edit to preserve relationships', () => {
     node: {
       row: {id: 1, title: 'issue1'},
       relationships: {
-        labels: [
+        labels: () => [
           {
             row: {id: 1, name: 'label1'},
             relationships: {},
@@ -1262,7 +1427,7 @@ test('edit to preserve relationships', () => {
     node: {
       row: {id: 2, title: 'issue2'},
       relationships: {
-        labels: [
+        labels: () => [
           {
             row: {id: 2, name: 'label2'},
             relationships: {},

packages/zql/src/ivm/catch.ts
@@ -1,8 +1,23 @@
 import {unreachable} from '../../../shared/src/asserts.ts';
 import type {Row} from '../../../zero-protocol/src/data.ts';
-import type {AddChange, Change, RemoveChange} from './change.ts';
+import type {Change} from './change.ts';
 import type {Node} from './data.ts';
 import type {FetchRequest, Input, Output} from './operator.ts';
+
+export type CaughtNode = {
+  row: Row;
+  relationships: Record<string, CaughtNode[]>;
+};
+
+export type CaughtAddChange = {
+  type: 'add';
+  node: CaughtNode;
+};
+
+export type CaughtRemoveChange = {
+  type: 'remove';
+  node: CaughtNode;
+};
 
 export type CaughtChildChange = {
   type: 'child';
@@ -20,8 +35,8 @@ export type CaughtEditChange = {
 };
 
 export type CaughtChange =
-  | AddChange
-  | RemoveChange
+  | CaughtAddChange
+  | CaughtRemoveChange
   | CaughtChildChange
   | CaughtEditChange;
 
@@ -75,7 +90,8 @@ export function expandChange(change: Change): CaughtChange {
       };
     case 'child':
       return {
-        ...change,
+        type: 'child',
+        row: change.node.row,
         child: {
           ...change.child,
           change: expandChange(change.child.change),
@@ -86,13 +102,13 @@ export function expandChange(change: Change): CaughtChange {
   }
 }
 
-export function expandNode(node: Node): Node {
+export function expandNode(node: Node): CaughtNode {
   return {
-    ...node,
+    row: node.row,
     relationships: Object.fromEntries(
       Object.entries(node.relationships).map(([k, v]) => [
         k,
-        [...v].map(expandNode),
+        [...v()].map(expandNode),
       ]),
     ),
   };

packages/zql/src/ivm/change.ts
@@ -1,4 +1,3 @@
-import type {Row} from '../../../zero-protocol/src/data.ts';
 import type {Node} from './data.ts';
 
 export type Change = AddChange | RemoveChange | ChildChange | EditChange;
@@ -24,11 +23,13 @@ export type RemoveChange = {
 };
 
 /**
- * The node itself is unchanged, but one of its descendants has changed.
+ * The node's row is unchanged, but one of its descendants has changed.
+ * The node's relationships will reflect the change, `child` specifies the
+ * specific descendant change.
  */
 export type ChildChange = {
   type: 'child';
-  row: Row;
+  node: Node;
   child: {
     relationshipName: string;
     change: Change;
@@ -62,8 +63,3 @@ export type EditChange = {
   node: Node;
   oldNode: Node;
 };
-
-export function rowForChange(change: Change): Row {
-  const {type} = change;
-  return type === 'child' ? change.row : change.node.row;
-}

packages/zql/src/ivm/data.ts
@@ -14,7 +14,7 @@ import type {Stream} from './stream.ts';
  */
 export type Node = {
   row: Row;
-  relationships: Record<string, Stream<Node>>;
+  relationships: Record<string, () => Stream<Node>>;
 };
 
 /**
@@ -108,4 +108,12 @@ export function valuesEqual(a: Value, b: Value): boolean {
     return false;
   }
   return a === b;
+}
+
+export function drainStreams(node: Node) {
+  for (const stream of Object.values(node.relationships)) {
+    for (const node of stream()) {
+      drainStreams(node);
+    }
+  }
 }

commit 3c0b22cd162a3f18418e65aff367dbd1ea068146

fix(zql): join should add relationship to child change pushes (#3746)
This impacted queries with multiple whereExist conditions.  The missing relationships on child changes would cause an assertion error in the Exist operator [here](https://github.com/rocicorp/mono/blob/23b57636e7bf9bc51c933e87d3dcfff63cf73843/packages/zql/src/ivm/exists.ts#L277).  This bug was introduced in https://github.com/rocicorp/mono/pull/3629

Issue reported on Discord https://discord.com/channels/830183651022471199/1288232858795769917/1338368796825620531

packages/zql/src/ivm/join.ts
@@ -150,7 +150,15 @@ export class Join implements Input {
         });
         break;
       case 'child':
-        this.#output.push(change);
+        this.#output.push({
+          type: 'child',
+          node: this.#processParentNode(
+            change.node.row,
+            change.node.relationships,
+            'fetch',
+          ),
+          child: change.child,
+        });
         break;
       case 'edit': {
         // When an edit comes in we need to:

packages/zql/src/builder/builder.test.ts
@@ -1328,6 +1328,215 @@ test('exists junction', () => {
   `);
 });
 
+test('duplicative exists junction', () => {
+  const {sources, getSource} = testSources();
+  const sink = new Catch(
+    buildPipeline(
+      {
+        table: 'users',
+        orderBy: [['id', 'asc']],
+        limit: 2,
+        where: {
+          type: 'and',
+          conditions: [
+            {
+              type: 'correlatedSubquery',
+              related: {
+                system: 'client',
+                correlation: {parentField: ['id'], childField: ['userID']},
+                subquery: {
+                  table: 'userStates',
+                  alias: 'zsubq_userStates',
+                  orderBy: [
+                    ['userID', 'asc'],
+                    ['stateCode', 'asc'],
+                  ],
+                },
+              },
+              op: 'EXISTS',
+            },
+            {
+              type: 'correlatedSubquery',
+              related: {
+                system: 'client',
+                correlation: {parentField: ['id'], childField: ['userID']},
+                subquery: {
+                  table: 'userStates',
+                  alias: 'zsubq_userStates',
+                  orderBy: [
+                    ['userID', 'asc'],
+                    ['stateCode', 'asc'],
+                  ],
+                },
+              },
+              op: 'EXISTS',
+            },
+          ],
+        },
+      },
+      {
+        getSource,
+        createStorage: () => new MemoryStorage(),
+      },
+    ),
+  );
+
+  expect(sink.fetch()).toMatchInlineSnapshot(`
+    [
+      {
+        "relationships": {
+          "zsubq_userStates_0": [
+            {
+              "relationships": {},
+              "row": {
+                "stateCode": "HI",
+                "userID": 1,
+              },
+            },
+          ],
+          "zsubq_userStates_1": [
+            {
+              "relationships": {},
+              "row": {
+                "stateCode": "HI",
+                "userID": 1,
+              },
+            },
+          ],
+        },
+        "row": {
+          "id": 1,
+          "name": "aaron",
+          "recruiterID": null,
+        },
+      },
+      {
+        "relationships": {
+          "zsubq_userStates_0": [
+            {
+              "relationships": {},
+              "row": {
+                "stateCode": "AZ",
+                "userID": 3,
+              },
+            },
+            {
+              "relationships": {},
+              "row": {
+                "stateCode": "CA",
+                "userID": 3,
+              },
+            },
+          ],
+          "zsubq_userStates_1": [
+            {
+              "relationships": {},
+              "row": {
+                "stateCode": "AZ",
+                "userID": 3,
+              },
+            },
+            {
+              "relationships": {},
+              "row": {
+                "stateCode": "CA",
+                "userID": 3,
+              },
+            },
+          ],
+        },
+        "row": {
+          "id": 3,
+          "name": "greg",
+          "recruiterID": 1,
+        },
+      },
+    ]
+  `);
+
+  // erik moves to hawaii
+  sources.userStates.push({type: 'add', row: {userID: 2, stateCode: 'HI'}});
+
+  expect(sink.pushes).toMatchInlineSnapshot(`
+    [
+      {
+        "node": {
+          "relationships": {
+            "zsubq_userStates_0": [
+              {
+                "relationships": {},
+                "row": {
+                  "stateCode": "AZ",
+                  "userID": 3,
+                },
+              },
+              {
+                "relationships": {},
+                "row": {
+                  "stateCode": "CA",
+                  "userID": 3,
+                },
+              },
+            ],
+            "zsubq_userStates_1": [
+              {
+                "relationships": {},
+                "row": {
+                  "stateCode": "AZ",
+                  "userID": 3,
+                },
+              },
+              {
+                "relationships": {},
+                "row": {
+                  "stateCode": "CA",
+                  "userID": 3,
+                },
+              },
+            ],
+          },
+          "row": {
+            "id": 3,
+            "name": "greg",
+            "recruiterID": 1,
+          },
+        },
+        "type": "remove",
+      },
+      {
+        "node": {
+          "relationships": {
+            "zsubq_userStates_0": [
+              {
+                "relationships": {},
+                "row": {
+                  "stateCode": "HI",
+                  "userID": 2,
+                },
+              },
+            ],
+            "zsubq_userStates_1": [
+              {
+                "relationships": {},
+                "row": {
+                  "stateCode": "HI",
+                  "userID": 2,
+                },
+              },
+            ],
+          },
+          "row": {
+            "id": 2,
+            "name": "erik",
+            "recruiterID": 1,
+          },
+        },
+        "type": "add",
+      },
+    ]
+  `);
+});
+
 test('exists junction with limit, remove row after limit, and last row', () => {
   const {sources, getSource} = testSources();
   const sink = new Catch(

packages/zql/src/query/query-impl.query.test.ts
@@ -1199,6 +1199,85 @@ test('where exists', () => {
   expect(materialized.data).toEqual([]);
 });
 
+test('duplicative where exists', () => {
+  const queryDelegate = new QueryDelegateImpl();
+  const issueSource = must(queryDelegate.getSource('issue'));
+  const labelSource = must(queryDelegate.getSource('label'));
+  const issueLabelSource = must(queryDelegate.getSource('issueLabel'));
+  issueSource.push({
+    type: 'add',
+    row: {
+      id: '0001',
+      title: 'issue 1',
+      description: 'description 1',
+      closed: false,
+      ownerId: '0001',
+    },
+  });
+  issueSource.push({
+    type: 'add',
+    row: {
+      id: '0002',
+      title: 'issue 2',
+      description: 'description 2',
+      closed: true,
+      ownerId: '0002',
+    },
+  });
+  labelSource.push({
+    type: 'add',
+    row: {
+      id: '0001',
+      name: 'bug',
+    },
+  });
+
+  const materialized = newQuery(queryDelegate, schema, 'issue')
+    .where('closed', true)
+    .whereExists('labels', q => q.where('name', 'bug'))
+    .whereExists('labels', q => q.where('name', 'bug'))
+    .related('labels')
+    .materialize();
+
+  expect(materialized.data).toEqual([]);
+
+  issueLabelSource.push({
+    type: 'add',
+    row: {
+      issueId: '0002',
+      labelId: '0001',
+    },
+  });
+
+  expect(materialized.data).toMatchInlineSnapshot(`
+    [
+      {
+        "closed": true,
+        "description": "description 2",
+        "id": "0002",
+        "labels": [
+          {
+            "id": "0001",
+            "name": "bug",
+          },
+        ],
+        "ownerId": "0002",
+        "title": "issue 2",
+      },
+    ]
+  `);
+
+  issueLabelSource.push({
+    type: 'remove',
+    row: {
+      issueId: '0002',
+      labelId: '0001',
+    },
+  });
+
+  expect(materialized.data).toEqual([]);
+});
+
 test('where exists before where, see https://bugs.rocicorp.dev/issue/3417', () => {
   const queryDelegate = new QueryDelegateImpl();
   const issueSource = must(queryDelegate.getSource('issue'));