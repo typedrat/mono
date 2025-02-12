36c22d85a36a71a3734cec6f823089f4ec6e918f
Aaron Boodman <aaron@aaronboodman.com>
2025-02-11 21:04:31 -1000

    Add new `make-latest.js` script

---
+++ packages/zero/tool/make-latest.js
@@ -0,0 +1,39 @@
+//@ts-check
+
+import {execSync} from 'node:child_process';
+
+/**
+ * @param {string} command
+ * @param {{stdio?:'inherit'|'pipe'|undefined, cwd?:string|undefined}|undefined} [options]
+ */
+function execute(command, options) {
+  console.log(`Executing: ${command}`);
+  return execSync(command, {stdio: 'inherit', ...options})
+    ?.toString()
+    ?.trim();
+}
+
+if (process.argv.length < 3) {
+  console.error(`Usage: node make-latest.js <npm-version>`);
+  process.exit(1);
+}
+
+const version = process.argv[2];
+
+execute(
+  `docker buildx imagetools create -t rocicorp/zero:latest rocicorp/zero:${version}`,
+);
+execute(`npm dist-tag add @rocicorp/zero@${version} latest`);
+
+console.log(``);
+console.log(``);
+console.log(`🎉 Success!`);
+console.log(``);
+console.log(`* Added 'latest' tag to @rocicorp/zero@${version} on npm.`);
+console.log(`* Added 'latest' tag to rocicorp/zero:${version} on dockerhub.`);
+console.log(``);
+console.log(``);
+console.log(`Next steps:`);
+console.log(``);
+console.log('* Bump version om main if necessary.');
+console.log(``);
