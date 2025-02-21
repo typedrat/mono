33bdb273a7efd8dd5c36d95eb853c0563ecffd50
Darick Tong <132324914+darkgnotic@users.noreply.github.com>
2025-02-21 09:21:00 -0800

    chore(prod): fix formatting (#3851)

--- prod/functions/src/permissions.ts
+++ prod/functions/src/permissions.ts
@@ -1,4 +1,4 @@
-import { execSync } from "node:child_process";
+import {execSync} from 'node:child_process';
 
 export const deploy = () =>
-  execSync("npx zero-deploy-permissions", { stdio: "inherit" });
+  execSync('npx zero-deploy-permissions', {stdio: 'inherit'});
