c9bd341c12750ab509ee1ce63eff5f8e9c695742
Aaron Boodman <aaron@aaronboodman.com>
2025-03-02 23:25:19 -1000

    Only debug transformed queries when added

--- packages/zero-cache/src/services/view-syncer/view-syncer.ts
+++ packages/zero-cache/src/services/view-syncer/view-syncer.ts
@@ -735,6 +735,15 @@ export class ViewSyncerService implements ViewSyncer, ActivityBasedService {
         transformationHash => !desiredQueries.has(transformationHash),
       );
 
+      for (const q of addQueries) {
+        lc.debug?.(
+          'ViewSyncer adding query',
+          q.ast,
+          'transformed from',
+          cvr.queries[q.id].ast,
+        );
+      }
+
       if (
         addQueries.length > 0 ||
         removeQueries.length > 0 ||
