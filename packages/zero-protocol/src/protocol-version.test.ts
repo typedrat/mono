d4102ca6ab69eafd7a4c6bd4dfd9e776ee4d30d9
Darick Tong <132324914+darkgnotic@users.noreply.github.com>
2025-03-05 12:56:45 -0800

    chore(zero-protocol): add test to flag when PROTOCOL_VERSION might need to be bumped (#3925)

---
+++ packages/zero-protocol/src/protocol-version.test.ts
@@ -0,0 +1,16 @@
+import {expect, test} from 'vitest';
+import {h64} from '../../shared/src/hash.ts';
+import {downstreamSchema} from './down.ts';
+import {PROTOCOL_VERSION} from './protocol-version.ts';
+import {upstreamSchema} from './up.ts';
+
+test('protocol version', () => {
+  const schemaJSON = JSON.stringify({upstreamSchema, downstreamSchema});
+  const hash = h64(schemaJSON).toString(36);
+
+  // If this test fails upstream or downstream schema has changed such that
+  // old code will not understand the new schema, bump the
+  // PROTOCOL_VERSION and update the expected values.
+  expect(hash).toEqual('1s66ur0ylpggf');
+  expect(PROTOCOL_VERSION).toEqual(6);
+});
