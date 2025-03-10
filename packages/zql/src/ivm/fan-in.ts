f32a2e0a0287e107b8fee93174d37ae061602153
Matt Wonlaw <mlaw@roci.dev>
2025-03-10 13:43:50 -0400

    bug(zql): simplify fix for fan-in/fan-out
    now we assume:
    1. child changes can only ever be converted to add/remove by exists
    2. exists always uses a unique relationship

--- packages/zql/src/ivm/fan-in.ts
+++ packages/zql/src/ivm/fan-in.ts
@@ -1,7 +1,7 @@
 import {assert} from '../../../shared/src/asserts.ts';
 import {mergeIterables} from '../../../shared/src/iterables.ts';
 import {must} from '../../../shared/src/must.ts';
-import type {Change, EditChange} from './change.ts';
+import type {Change} from './change.ts';
 import type {Node} from './data.ts';
 import type {FanOut} from './fan-out.ts';
 import {
@@ -82,7 +82,7 @@ export class FanIn implements Operator {
   fanOutDonePushingToAllBranches(fanOutChangeType: Change['type']) {
     if (this.#inputs.length === 0) {
       assert(
-        this.#inputs.length === 0,
+        this.#accumulatedPushes.length === 0,
         'If there are no inputs then fan-in should not receive any pushes.',
       );
       return;
@@ -91,95 +91,19 @@ export class FanIn implements Operator {
     if (this.#accumulatedPushes.length === 0) {
       // It is possible for no forks to pass along the push.
       // E.g., if no filters match in any fork.
-      // This can happen for all change types: add, remove, edit, child.
-      // In `edit`, both the old and new rows can be filtered out.
-      // In `child`, the parent row can be filtered out.
-      // In `add` and `remove`, the row itself can be filtered out.
       return;
     }
 
-    // We collect changes for each type to determine what to push.
-    // We do not need all changes for each type as the changes for most types are expected
-    // to be identical.
-    //
-    // Exists is a subtle case, however. A `child` change coming into `fanOut` can be
-    // converted into a `remove` or `add` where the `relationships` have been modified by `exists`.
-    //
-    //    | type:child
-    //  fanOut
-    //    | type:child
-    //   exists
-    //    | type:remove
-    //   fanIn
-    //
-    // In this case, the changes must be merged to preserve the `relationship` editing done by `exists`.
-    //
-    //
-    // We only collect 1 change for each type. It would be incorrect to output the same change type
-    // many times. See the comment at the bottom of this file.
-    //
-    // >> What happens if we ever allow `join` in between `fan-in`, `fan-out`? <<
-    //    That would be nonsense. It would mean the shape of the query result is
-    //    different depending on which branches evaluated to true.
-    //
+    // collapse down to a single change per type
     const candidatesToPush = new Map<Change['type'], Change>();
     for (const change of this.#accumulatedPushes) {
-      const existing = candidatesToPush.get(change.type);
-      if (existing !== undefined) {
-        if (existing.node === change.node) {
-          // 1. We have recorded a change for this type
-          // 2. The nodes are identical
-          // Can skip this change.
-          continue;
-        }
-
+      if (fanOutChangeType === 'child' && change.type !== 'child') {
         assert(
-          existing.node.row === change.node.row,
-          'Expected identical rows',
+          candidatesToPush.has(change.type) === false,
+          () =>
+            `Fan-in:child expected at most one ${change.type} when fan-out is of type child`,
         );
-
-        // This is the nuanced case of `exists` modifying the relationships in a `child` change.
-        assert(
-          fanOutChangeType === 'child' &&
-            // NOTE: you can get `edit` here for nested ors
-            (change.type === 'add' ||
-              change.type === 'remove' ||
-              change.type === 'edit'),
-          'Expected child change',
-        );
-        const replace: Change =
-          change.type === 'edit'
-            ? {
-                type: 'edit',
-                node: {
-                  row: change.node.row,
-                  relationships: {
-                    ...existing.node.relationships,
-                    ...change.node.relationships,
-                  },
-                },
-                oldNode: {
-                  row: change.oldNode.row,
-                  relationships: {
-                    ...(existing as EditChange).oldNode.relationships,
-                    ...change.oldNode.relationships,
-                  },
-                },
-              }
-            : {
-                type: change.type,
-                node: {
-                  row: change.node.row,
-                  relationships: {
-                    ...existing.node.relationships,
-                    ...change.node.relationships,
-                  },
-                },
-              };
-        candidatesToPush.set(change.type, replace);
-        continue;
       }
-
       candidatesToPush.set(change.type, change);
     }
 
@@ -192,18 +116,21 @@ export class FanIn implements Operator {
      * - remove must result in all removes
      * - add must result in all adds
      * - edit must result in add or removes or edits
-     * - child...
+     * - child must result in a single add or single remove or many child changes
      */
     switch (fanOutChangeType) {
       case 'remove':
         assert(
           types.length === 1 && types[0] === 'remove',
-          'Expected all removes',
+          'Fan-in:remove expected all removes',
         );
         this.#output.push(must(candidatesToPush.get('remove')));
         return;
       case 'add':
-        assert(types.length === 1 && types[0] === 'add', 'Expected all adds');
+        assert(
+          types.length === 1 && types[0] === 'add',
+          'Fan-in:add expected all adds',
+        );
         this.#output.push(must(candidatesToPush.get('add')));
         return;
       case 'edit': {
@@ -211,7 +138,7 @@ export class FanIn implements Operator {
           types.every(
             type => type === 'add' || type === 'remove' || type === 'edit',
           ),
-          'Expected all adds, removes, or edits',
+          'Fan-in:edit expected all adds, removes, or edits',
         );
         const addChange = candidatesToPush.get('add');
         const removeChange = candidatesToPush.get('remove');
@@ -242,7 +169,6 @@ export class FanIn implements Operator {
         // The left filter converts the edit into a remove.
         // The right filter converts the edit into an add.
         if (addChange && removeChange) {
-          // convert back to edit
           this.#output.push({
             type: 'edit',
             node: addChange.node,
@@ -255,49 +181,34 @@ export class FanIn implements Operator {
         return;
       }
       case 'child': {
-        // complex case due to exists
-        // one branch could add and another remove or vice-versa.
         assert(
           types.every(
             type =>
               type === 'add' || // exists can change child to add or remove
               type === 'remove' || // exists can change child to add or remove
-              type === 'child' || // other operators may preserve the child change
-              type === 'edit', // edit can show up in the case of nested `or` expressions.
+              type === 'child', // other operators may preserve the child change
           ),
-          'Expected all adds, removes, or children',
+          'Fan-in:child expected all adds, removes, or children',
+        );
+        assert(
+          types.length <= 2,
+          'Fan-in:child expected at most 2 types on a child change from fan-out',
         );
 
         // If any branch preserved the original child change, that takes precedence over all other changes.
-        // This is due to the nature of `or`. Explained at the bottom of this file.
         const childChange = candidatesToPush.get('child');
         if (childChange) {
           this.#output.push(childChange);
           return;
         }
 
-        const editChange = candidatesToPush.get('edit');
         const addChange = candidatesToPush.get('add');
         const removeChange = candidatesToPush.get('remove');
 
-        if (editChange) {
-          this.#output.push(editChange);
-          return;
-        }
-
-        if (addChange && removeChange) {
-          // convert to an edit
-          // hm.... do we want to do this?
-          // If we do not, this results in a "split push" -- child splitting to many changes.
-          // If we do, this means an `edit` can show up in this case if `fan-in` and `fan-out` are nested
-          // within an outer `fan-in` and `fan-out`.
-          this.#output.push({
-            type: 'edit',
-            node: addChange.node,
-            oldNode: removeChange.node,
-          } as const);
-          return;
-        }
+        assert(
+          addChange === undefined || removeChange === undefined,
+          'Fan-in:child expected either add or remove, not both',
+        );
 
         this.#output.push(must(addChange ?? removeChange));
         return;
