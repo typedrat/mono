49c0a4462a94fc06e0a92de5ceabc7e222aa7bdb
Matt Wonlaw <mlaw@roci.dev>
2025-04-11 11:52:23 -0400

    feat!(zbugs): remove all write permissions
    zubgs strictly uses custom mutators. We do not need write permissions anymore.

--- apps/zbugs/shared/schema.ts
+++ apps/zbugs/shared/schema.ts
@@ -4,7 +4,6 @@ import {
   createSchema,
   definePermissions,
   enumeration,
-  NOBODY_CAN,
   number,
   relationships,
   string,
@@ -13,7 +12,6 @@ import {
   type Row,
 } from '@rocicorp/zero';
 import type {AuthData} from './auth.ts';
-import type {Condition} from 'zero-protocol/src/ast.js';
 
 // Table definitions
 const user = table('user')
@@ -203,32 +201,12 @@ export type IssueRow = Row<typeof schema.tables.issue>;
 export type CommentRow = Row<typeof schema.tables.comment>;
 export type UserRow = Row<typeof schema.tables.user>;
 
-type PermissionRule<TTable extends TableName> = (
-  authData: AuthData,
-  eb: ExpressionBuilder<Schema, TTable>,
-) => Condition;
-
-function and<TTable extends TableName>(
-  ...rules: PermissionRule<TTable>[]
-): PermissionRule<TTable> {
-  return (authData, eb) => eb.and(...rules.map(rule => rule(authData, eb)));
-}
-
 export const permissions: ReturnType<typeof definePermissions> =
   definePermissions<AuthData, Schema>(schema, () => {
     const userIsLoggedIn = (
       authData: AuthData,
       {cmpLit}: ExpressionBuilder<Schema, TableName>,
     ) => cmpLit(authData.sub, 'IS NOT', null);
-
-    const loggedInUserIsCreator = (
-      authData: AuthData,
-      eb: ExpressionBuilder<Schema, 'comment' | 'emoji' | 'issue'>,
-    ) =>
-      eb.and(
-        userIsLoggedIn(authData, eb),
-        eb.cmp('creatorID', '=', authData.sub),
-      );
 
     const loggedInUserIsAdmin = (
       authData: AuthData,
@@ -244,158 +222,50 @@ export const permissions: ReturnType<typeof definePermissions> =
       {cmp}: ExpressionBuilder<Schema, 'viewState' | 'userPref'>,
     ) => cmp('userID', '=', authData.sub);
 
-    const allowIfAdminOrIssueCreator = (
-      authData: AuthData,
-      eb: ExpressionBuilder<Schema, 'issueLabel'>,
-    ) =>
-      eb.or(
-        loggedInUserIsAdmin(authData, eb),
-        eb.exists('issue', iq =>
-          iq.where(eb => loggedInUserIsCreator(authData, eb)),
-        ),
-      );
-
     const canSeeIssue = (
       authData: AuthData,
       eb: ExpressionBuilder<Schema, 'issue'>,
     ) =>
       eb.or(loggedInUserIsAdmin(authData, eb), eb.cmp('visibility', 'public'));
 
-    /**
-     * Comments are only visible if the user can see the issue they're attached to.
-     */
-    const canSeeComment = (
-      authData: AuthData,
-      eb: ExpressionBuilder<Schema, 'comment'>,
-    ) => eb.exists('issue', q => q.where(eb => canSeeIssue(authData, eb)));
-
-    /**
-     * Issue labels are only visible if the user can see the issue they're attached to.
-     */
-    const canSeeIssueLabel = (
-      authData: AuthData,
-      eb: ExpressionBuilder<Schema, 'issueLabel'>,
-    ) => eb.exists('issue', q => q.where(eb => canSeeIssue(authData, eb)));
-
-    /**
-     * Emoji are only visible if the user can see the issue they're attached to.
-     */
-    const canSeeEmoji = (
-      authData: AuthData,
-      {exists, or}: ExpressionBuilder<Schema, 'emoji'>,
-    ) =>
-      or(
-        exists('issue', q => {
-          return q.where(eb => canSeeIssue(authData, eb));
-        }),
-        exists('comment', q => {
-          return q.where(eb => canSeeComment(authData, eb));
-        }),
-      );
-
     return {
       user: {
-        // Only the authentication system can write to the user table.
         row: {
-          insert: NOBODY_CAN,
-          update: {
-            preMutation: NOBODY_CAN,
-          },
-          delete: NOBODY_CAN,
           select: ANYONE_CAN,
         },
       },
       issue: {
         row: {
-          insert: [
-            // prevents setting the creatorID of an issue to someone
-            // other than the user doing the creating
-            loggedInUserIsCreator,
-          ],
-          update: {
-            preMutation: [loggedInUserIsCreator, loggedInUserIsAdmin],
-            postMutation: [loggedInUserIsCreator, loggedInUserIsAdmin],
-          },
-          delete: [loggedInUserIsCreator, loggedInUserIsAdmin],
           select: [canSeeIssue],
         },
       },
       comment: {
         row: {
-          insert: [
-            loggedInUserIsAdmin,
-            and(loggedInUserIsCreator, canSeeComment),
-          ],
-          update: {
-            preMutation: [
-              loggedInUserIsAdmin,
-              and(loggedInUserIsCreator, canSeeComment),
-            ],
-            postMutation: [
-              loggedInUserIsAdmin,
-              and(loggedInUserIsCreator, canSeeComment),
-            ],
-          },
-          delete: [
-            loggedInUserIsAdmin,
-            and(canSeeComment, loggedInUserIsCreator),
-          ],
           select: ANYONE_CAN,
         },
       },
       label: {
         row: {
-          insert: [loggedInUserIsAdmin],
-          update: {
-            preMutation: [loggedInUserIsAdmin],
-          },
-          delete: [loggedInUserIsAdmin],
           select: ANYONE_CAN,
         },
       },
       viewState: {
         row: {
-          insert: [allowIfUserIDMatchesLoggedInUser],
-          update: {
-            preMutation: [allowIfUserIDMatchesLoggedInUser],
-            postMutation: [allowIfUserIDMatchesLoggedInUser],
-          },
-          delete: NOBODY_CAN,
           select: ANYONE_CAN,
         },
       },
       issueLabel: {
         row: {
-          insert: [and(canSeeIssueLabel, allowIfAdminOrIssueCreator)],
-          update: {
-            preMutation: NOBODY_CAN,
-          },
-          delete: [and(canSeeIssueLabel, allowIfAdminOrIssueCreator)],
           select: ANYONE_CAN,
         },
       },
       emoji: {
         row: {
-          // Can only insert emoji if the can see the issue.
-          insert: [and(canSeeEmoji, loggedInUserIsCreator)],
-
-          // Can only update their own emoji.
-          update: {
-            preMutation: [and(canSeeEmoji, loggedInUserIsCreator)],
-            postMutation: [and(canSeeEmoji, loggedInUserIsCreator)],
-          },
-          delete: [and(canSeeEmoji, loggedInUserIsCreator)],
           select: ANYONE_CAN,
         },
       },
       userPref: {
         row: {
-          insert: [allowIfUserIDMatchesLoggedInUser],
-          update: {
-            preMutation: [allowIfUserIDMatchesLoggedInUser],
-            postMutation: [allowIfUserIDMatchesLoggedInUser],
-          },
-          delete: [allowIfUserIDMatchesLoggedInUser],
           select: [allowIfUserIDMatchesLoggedInUser],
         },
       },
