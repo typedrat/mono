86b8f18ada01276383282718f956cafd0615604b
Erik Arvidsson <arv@roci.dev>
2025-05-16 10:55:14 +0200

    Update version test

--- packages/zero-cache/src/services/change-source/protocol/version.test.ts
+++ packages/zero-cache/src/services/change-source/protocol/version.test.ts
@@ -38,9 +38,9 @@ test('protocol versions', () => {
   // Then update the version number of the `CHANGE_SOURCE_PATH`
   // in current and export it appropriately as the new version
   // in `mod.ts`.
-  t(current, '2v7p7445ss8ce', '/changes/v0/stream');
+  t(current, 'hfkz08pmaavz', '/changes/v0/stream');
   // During initial development, we use v0 as a non-stable
   // version (i.e. breaking change are allowed). Once the
   // protocol graduates to v1, versions must be stable.
-  t(v0, '2v7p7445ss8ce', '/changes/v0/stream');
+  t(v0, 'hfkz08pmaavz', '/changes/v0/stream');
 });
